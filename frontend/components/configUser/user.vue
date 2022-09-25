2a19f020a497cfe02342873893225394e10e9760
Eduarda Saibert <eduarda6saibert@gmail.com>
2022-09-25 16:44:41 -0300

    Dialog (desktop view) fix

--- frontend/components/configUser/user.vue
+++ frontend/components/configUser/user.vue
@@ -14,24 +14,24 @@
     <form @submit.prevent class="row-wrapper">
       <div class="box-config-user d-flex flex-row align-center">
         <div class="header-title me-3">Nickname:</div>
-        <v-text-field value="" v-model="userLocal.username" dark></v-text-field>
+        <v-text-field value="" class="text-field" v-model="userLocal.username" dark></v-text-field>
       </div>
       <div class="box-config-user d-flex flex-row align-center">
         <div class="header-title me-3">Nome:</div>
-        <v-text-field value="" v-model="userLocal.first_name" dark></v-text-field>
+        <v-text-field value="" class="text-field" v-model="userLocal.first_name" dark></v-text-field>
       </div>
       <div class="box-config-user d-flex flex-row align-center">
         <div class="header-title me-3">Sobrenome:</div>
-        <v-text-field value="" v-model="userLocal.last_name" dark></v-text-field>
+        <v-text-field value="" class="text-field" v-model="userLocal.last_name" dark></v-text-field>
       </div>
       <div class="box-config-user d-flex flex-row align-center">
         <div class="header-title me-3">E-mail:</div>
-        <v-text-field value="" v-model="userLocal.email" dark></v-text-field>
+        <v-text-field value="" class="text-field" v-model="userLocal.email" dark></v-text-field>
       </div>
       <!--
       <div class="box-config-user d-flex flex-row align-center">
         <div class="header-title me-3">Senha:</div>
-        <v-text-field value="" v-model="userLocal.password" dark></v-text-field>
+        <v-text-field value="" class="text-field" v-model="userLocal.password" dark></v-text-field>
       </div>
       !-->
     </form>
@@ -41,6 +41,7 @@
         <div class="header-title me-3">Biografia:</div>
         <v-textarea
             name="Biografia"
+            class="text-field"
             auto-grow
             v-model="userLocal.user_biography"
             value=""
@@ -54,23 +55,22 @@
     <form @submit.prevent class="row-wrapper">
       <div class="box-config-user d-flex flex-row align-center">
         <div class="header-title me-3">País:</div>
-        <v-text-field value="Lugar nenhum" dark></v-text-field>
+        <v-text-field value="Lugar nenhum" class="text-field" dark></v-text-field>
       </div>
       <div class="box-config-user d-flex flex-row align-center">
         <div class="header-title me-3">Estado:</div>
-        <v-text-field value="Lugar nenhum" dark></v-text-field>
+        <v-text-field value="Lugar nenhum" class="text-field" dark></v-text-field>
       </div>
       <div class="box-config-user d-flex flex-row align-center">
         <div class="header-title me-3">Cidade:</div>
-        <v-text-field value="Lugar nenhum" dark></v-text-field>
+        <v-text-field value="Lugar nenhum" class="text-field" dark></v-text-field>
       </div>
     </form>
-    <div class="button d-flex flex-row align-center justify-end me-3">
+    <v-row class="box-config-user d-flex flex-row justify-end me-3">
+      <button class="btnWormz special mt-5 ms-3" type="button" @click="deleteUser(), $emit('fechar')">DELETAR PERFIL</button>
+    </v-row>
+    <div class="button d-flex flex-row align-center justify-end me-3 mt-10">
       <button class="btnWormz ms-3" type="button" @click="submitAuthData">SALVAR</button>
-    </div>
-    <div class="box-config-user d-flex flex-row align-center">
-      <div class="header-title ms-3">Deletar Perfil</div>
-      <button class="btnWormz special mt-5 ms-3" type="button" @click="deleteUser(), $emit('fechar')">DELETAR</button>
     </div>
   </div>
 </template>
@@ -83,8 +83,8 @@ export default {
       userLocal: {}
     }
   },
-  mounted(){
-    this.userLocal.id = this.user.id
+  created(){
+    this.userLocal = this.user
   },
   methods:{
     ...mapActions('auth', ['LOGOUT']),
@@ -131,5 +131,8 @@ export default {
 .special{
   background-color: red !important;
   color: white !important;
+}
+.text-field{
+  font-size: 2vh;
 }
 </style>
