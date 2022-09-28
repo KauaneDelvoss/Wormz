de22d7900247ef88b44b527c9c265c89915909f0
coffeau <eduarda6saibert@gmail.com>
2022-09-28 14:59:04 -0300

    dynamic page book fix

--- frontend/pages/bib/_book.vue
+++ frontend/pages/bib/_book.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="book-view">
-    <v-container fluid class="container">
-        <p>
-            <span class="h3">{{ book.volumeInfo.title }}</span>
-        </p>
-        <p>
-            <span class="subtitulo"> {{ book.volumeInfo.authors }}  </span>
-            <span class="subtitulo"> {{ book.volumeInfo.publishedDate }} </span>
-        </p>
-        {{ id }}
-    </v-container>
+        <v-row v-if="book.volumeInfo" class="d-flex container">
+            <v-col cols="6">
+                <div>
+                    <p>
+                        <span class="h3">{{ book.volumeInfo.title }}</span>
+                    </p>
+                </div>
+                <div>
+                    <div v-for="author in book.volumeInfo.authors" :key="author">
+                        <span class="subtitulo"> {{ author }} </span>
+                    </div>
+                    <span class="subtitulo data"> {{ book.volumeInfo.publishedDate }} </span>
+                </div>
+            </v-col>
+            <v-col cols="6" class="d-flex justify-end">
+                <v-card class="mx-auto book">
+                    <v-img
+                    v-if="
+                        book.volumeInfo.imageLinks
+                        ? book.volumeInfo.imageLinks.thumbnail
+                        : false
+                    "
+                    :src="book.volumeInfo.imageLinks.thumbnail"
+                    height="28vh"
+                    aspect-ratio="1.7"
+                    />
+                </v-card>
+            </v-col>
+        </v-row>
   </div>
 </template>
 
@@ -18,14 +37,12 @@ import { mapState } from 'vuex'
 export default {
     layout: 'bib',
     data(){
-        return{
-            id: '',
-            book: {}
-        }
+        return{}
     },
-    created(){
-        this.id = this.$router.currentRoute.params.id
-        this.$store.dispatch("book/GET_URL", this.id)
+    mounted(){
+        const id = this.$router.currentRoute.params.id
+        this.$store.dispatch("book/GET_URL", id)
+        console.log(this.book)
     },
     computed: {
         ...mapState('book', ['book'])
@@ -33,6 +50,17 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+.data{
+    font-size: 1.5vw;
+    opacity: 80%;
+}
 
+.book{
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    background-color: variables.$primary-color;
+    border-radius: 15px;
+    height: 10vw;
+    width: 9vw
+}
 </style>
--- frontend/store/book/index.js
+++ frontend/store/book/index.js
@@ -1,24 +1,23 @@
 export const state = () => ({
     book: {},
-    api_key: 'AIzaSyDb8Cue7PCPcACj9eba6p82EDDLHwXDNLk',
-
+    api_key: 'AIzaSyDb8Cue7PCPcACj9eba6p82EDDLHwXDNLk'
 })
 
 export const mutations = {
     COMMIT_URL(state, payload){
-        state.bookshelf = payload
+        console.log(payload)
+        state.book = payload
     },
 }
 
 export const actions = {
     GET_URL({state, commit}, id){
-            const url = 'https://www.googleapis.com/books/v1/volumes/' + id + "&key=" + state.api_key
+            const url = 'https://www.googleapis.com/books/v1/volumes/' + id + "?key=" + state.api_key
             this.$axios.$get(url, {headers: {
                 'Authorization': ''
             }})
             .then(res => { 
-                commit('COMMIT_URL',  res.items)
-                console.log(res)
+                commit('COMMIT_URL',  res)
             })
         }
     }
