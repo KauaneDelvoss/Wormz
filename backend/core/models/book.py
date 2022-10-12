8a893183396897fce32754eb12d2810170a2c11e
KauaneDelvoss <delvoss.ribas@gmail.com>
2022-10-12 14:49:38 -0300

    criação da model Book

--- backend/core/models/book.py
+++ backend/core/models/book.py
@@ -1,7 +1,15 @@
+
 from django.db import models
 
-class book(models.Model):
+class Book(models.Model):
     resume = models.CharField(max_length=2000, null=True)
     name_book = models.CharField(max_length=100)
     cod_book = models.AutoField(primary_key=True) #mudou de integerfield para autoincrement
-    year = models.YearField(null=True)
+    #year = models.YearField(null=True)
+    pages = models.IntegerField(null=True)
+
+    def __str__(self) :
+        return self.name_book
+
+    class Meta:
+        verbose_name_plural = "Books"
