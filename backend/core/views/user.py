cf64571d9ba7f0771066cd78331d10c3cbb1524a
KauaneDelvoss <delvoss.ribas@gmail.com>
2022-10-18 13:15:41 -0300

    def cadastro user

--- backend/core/views/user.py
+++ backend/core/views/user.py
@@ -8,4 +8,15 @@ class UserViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
+    def cadastro(request):
+        if request.method == "POST" :
+            username = request.POST.get('username')
+            nickname = request.POST.get('nickname')
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            user_biography = request.POST.get('user_biography')
+
+            
