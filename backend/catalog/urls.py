f57663b3e5e20e4156664f1e98ed54e0d4f3f156
KauaneDelvoss <delvoss.ribas@gmail.com>
2022-10-20 00:06:39 -0300

    .

--- backend/catalog/urls.py
+++ backend/catalog/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, BookViewSet, BookshelfViewSet, GenreViewSet, OwnViewSet, TypeViewSet, WritesViewSet,  cadastro,  getUser, updateUser, deleteUser
+from core.views import UserViewSet, BookViewSet, BookshelfViewSet, GenreViewSet, OwnViewSet, TypeViewSet, WritesViewSet
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
@@ -18,10 +18,10 @@ router.register(r"writes", WritesViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('cadastro/', cadastro, name="cadastro"),
-    path('get/user', getUser, name="getUser"),
-    path('update/user', updateUser, name="updateUser"),
-    path('delete/user', deleteUser, name="deleteUser"),
+    path('cadastro/', UserViewSet.cadastro, name="cadastro"),
+    #path('get/user', getUser, name="getUser"),
+    #path('update/user', updateUser, name="updateUser"),
+    #path('delete/user', deleteUser, name="deleteUser"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- backend/core/views/user.py
+++ backend/core/views/user.py
@@ -1,5 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User as UserDjangoAuth
+from django.contrib.auth.models import Group
+from django.http.response import HttpResponse
+from rest_framework.renderers import JSONRenderer
 
 from core.models import User
 from core.serializers import UserSerializer
@@ -19,4 +23,16 @@ class UserViewSet(ModelViewSet):
             last_name = request.POST.get('last_name')
             user_biography = request.POST.get('user_biography')
 
-            
+            if user:
+                return HttpResponse("Já existe um usuário com esse username!")
+            else:
+                user = UserDjangoAuth.objects.create_user(username = username, password = password, email=email, first_name=first_name, last_name=last_name)
+                id_user = (UserDjangoAuth.objects.get(username=username)).id
+                perfil = User(nickname=username, user_biography=user_biography, cod_user=id_user)
+
+                user.save()
+                perfil.save()
+
+                user_group = UserDjangoAuth.objects.get(username = username)
+                user_group.groups.add(group)
+                return HttpResponse('Usuário cadastrado com sucesso!')
