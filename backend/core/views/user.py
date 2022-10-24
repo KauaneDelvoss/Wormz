1aa3a6132219378ae8b568874ee48c502913ecde
Maria Laura <marialauraviana@gmail.com>
2022-10-24 09:20:36 -0300

    alteração user

--- backend/core/views/user.py
+++ backend/core/views/user.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
-#from django.contrib.auth.models import AbstractUser as DjangoAuthAdmin
+
+# from django.contrib.auth.models import AbstractUser as DjangoAuthAdmin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.http.response import HttpResponse
@@ -10,7 +11,8 @@ from django.shortcuts import render
 from core.models import User
 from core.serializers import UserSerializer, UserAuthSerializer
 
-#User = get_user_model()
+# User = get_user_model()
+
 
 class UserViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
@@ -19,67 +21,75 @@ class UserViewSet(ModelViewSet):
 
     def cadastro(request):
         if request.POST:
-            username = request.POST.get('username')
-            #nickname = request.POST.get('nickname')
-            password = request.POST.get('password')
-            email = request.POST.get('email')
-            first_name = request.POST.get('first_name')
-            last_name = request.POST.get('last_name')
-            user_biography = request.POST.get('user_biography')
+            username = request.POST.get("username")
+            # nickname = request.POST.get('nickname')
+            password = request.POST.get("password")
+            email = request.POST.get("email")
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            user_biography = request.POST.get("user_biography")
 
-            group = Group.objects.get(name='common_users') 
+            group = Group.objects.get(name="common_users")
             user = User.objects.filter(username=username).first()
 
             if user:
-                return render (HttpResponse("Já existe um usuário com esse username!"))
+                return render(HttpResponse("Já existe um usuário com esse username!"))
             else:
-                user = User.objects.create_user(username = username, password = password, email=email, first_name=first_name, last_name=last_name)
-                #id_user = (User.objects.get(username=username)).id
-                #perfil = User(username=username, user_biography=user_biography) #, cod_user=id_user)
-                user.is_active = True 
+                user = User.objects.create_user(
+                    username=username,
+                    password=password,
+                    email=email,
+                    first_name=first_name,
+                    last_name=last_name,
+                )
+                # id_user = (User.objects.get(username=username)).id
+                # perfil = User(username=username, user_biography=user_biography) #, cod_user=id_user)
+                user.is_active = True
                 user.save()
-                #perfil.save()
+                # perfil.save()
 
-                #user_group = User.objects.get(username = username)
-                #user_group.groups.add(group)
+                # user_group = User.objects.get(username = username)
+                # user_group.groups.add(group)
 
                 user.groups.add(group)
 
-                return render (HttpResponse('Usuário cadastrado com sucesso!'))
-                 
+                return HttpResponse("Usuário cadastrado com sucesso!")
+
     def getUser(request):
-        username = request.POST.get("username")
-        user = User.objects.get(username = username)
-        perfil = User.objects.get(username = username)
-        
+        # username = request.POST.get("username")
+        user = User.objects.get(username=request.POST.get("username"))
+        # perfil = User.objects.get(username = username)
+
         serializers = UserAuthSerializer(user)
         data = serializers.data
 
-        data.update(user_biography = perfil.user_biography)
+        data.update(username=user.username)
 
         json = JSONRenderer().render(data)
         return HttpResponse(json, content_type="text/json-comment-filtered")
 
     def updateUser(request):
         if request.POST:
-            user = User.objects.get(pk=request.POST.get('id'))
-            profile = User.objects.get(cod_user=request.POST.get('id'))
-            profile.user_biography = request.POST.get('user_biography')
-            user.username = request.POST.get('username')
-            user.first_name = request.POST.get('first_name')
-            user.last_name = request.POST.get('last_name')
-            profile.save()
+            user = User.objects.get(pk=request.POST.get("id"))
+            # profile = User.objects.get(cod_user=request.POST.get('id'))
+            user.user_biography = request.POST.get("user_biography")
+            user.username = request.POST.get("username")
+            user.first_name = request.POST.get("first_name")
+            user.last_name = request.POST.get("last_name")
+            user.email = request.POST.get("email")
+
+            # profile.save()
             user.save()
 
-            return HttpResponse('Usuário atualizado com sucesso!')
+            return HttpResponse("Usuário atualizado com sucesso!")
         else:
-            return HttpResponse('Métodos permitidos: /POST/')
+            return HttpResponse("Métodos permitidos: /POST/")
 
     def deleteUser(request):
         print(request)
 
-        user = User.objects.get(pk=request.POST.get('id'))
-        profile = User.objects.get(cod_user=request.POST.get('id'))
+        user = User.objects.get(pk=request.POST.get("id"))
+        #profile = User.objects.get(cod_user=request.POST.get("id"))
         user.delete()
-        profile.delete()
-        return HttpResponse('Usuário deletado com sucesso!')            
+        #profile.delete()
+        return HttpResponse("Usuário deletado com sucesso!")
