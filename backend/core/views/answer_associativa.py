b06d76dea26e3c4d960ae1a42fe460783f2dfced
KauaneDelvoss <delvoss.ribas@gmail.com>
2022-12-06 20:31:16 -0500

    def createQuiz

--- backend/core/views/answer_associativa.py
+++ backend/core/views/answer_associativa.py
@@ -1,9 +1,38 @@
+from rest_framework.renderers import JSONRenderer
+import json
+from django.http.response import HttpResponse
+
+from django.http import JsonResponse
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import AnswerAssociativa
+from core.models import AnswerAssociativa, User
 from core.serializers import AnswerAssociativaSerializer
 
 
 class AnswerAssociativaViewSet(ModelViewSet):
     queryset = AnswerAssociativa.objects.all()
     serializer_class = AnswerAssociativaSerializer
+
+    def createQuiz(request):
+        body = json.loads(request.body.decode('utf-8'))
+
+
+        print(body)
+        # cod_resp_forms = body[]
+        cod_answer = body["cod_answer"]
+        cod_question = body["cod_question"]
+        user = body["user"]
+
+        # print(user['id'])
+        user = User.objects.get(pk = user)
+        # user = User.objects.get(pk = user["id"])
+        quiz = AnswerAssociativa.objects.create(
+                    cod_resp_forms = user,
+                    cod_answer = cod_answer,
+                    cod_question = cod_question
+                )
+
+        quiz.save()
+
+        print(body)
+        return HttpResponse("deu certo?")
