d216560ba08e981df96b7c71c725cd5a991ee560
KauaneDelvoss <delvoss.ribas@gmail.com>
2022-12-12 18:11:19 -0500

    quiz

--- backend/core/views/answer_associativa.py
+++ backend/core/views/answer_associativa.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import AnswerAssociativa, User, Answer, Question, Genre, Form, Bookshelf
-from core.serializers import AnswerAssociativaSerializer
+from core.serializers import AnswerAssociativaSerializer, UserSerializer
 from core.views import BookshelfViewSet
 
 
@@ -16,12 +16,12 @@ class AnswerAssociativaViewSet(ModelViewSet):
 
     def createQuiz(request):
         body = json.loads(request.body.decode('utf-8'))
-        print(body)
+        # print(body)
         cod_answer = body["cod_answer"]
         cod_question = body["cod_question"]
         cod_form = body["cod_forms"]
         user = body["user"]
-        print(body)
+        # print(body)
         user = User.objects.get(pk = user)
 
         quiz = AnswerAssociativa.objects.create(
@@ -30,12 +30,35 @@ class AnswerAssociativaViewSet(ModelViewSet):
                     cod_question = Question.objects.get(id = cod_question ),
                 )
         quiz.cod_forms.add(Form.objects.get(name_forms = cod_form))
-        print(quiz)
+        # print(quiz)
         quiz.save()
 
-        print(body)
+        
+        # print(body)
         return HttpResponse(quiz)
 
     def BookshelfQuiz(request) :
-        pass
+        body = json.loads(request.body.decode('utf-8'))
+        bookshelf_name = 'Estante com livros indicados' 
+        bookshelf_desc = 'Estante com livros indicados'
+        user = body["user"]
+        users = User.objects.get(pk = user)
+
+        serializer = UserSerializer(users).data
+        # print(users.first_name)
+
+        info = {'bookshelf_name' : bookshelf_name , 'bookshelf_desc' : bookshelf_desc, 'user':  {'username': serializer['username'], 'password': serializer['password'], 'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcwNTE4NTMyLCJpYXQiOjE2NzA1MTgyMzIsImp0aSI6IjFkZjc1ZTYzM2Y3MjRiYzE5YTQ3NTBjNjY4MTM5ZjBlIiwidXNlcl9pZCI6MX0.QIiaIvvFb-7ZGIxwxakE7zULyieFk3Z8-zx4S_0VZlQ', 'id': serializer['id'], 'first_name': serializer['first_name'], 'last_name': serializer['last_name']}}
+        # print(info)
+
+        BookshelfViewSet.createBookshelfQuiz(json.dumps(info))
+
+
+        return HttpResponse("oi") 
+        # return HttpResponse(request)
+
+
+
+
+
+
 
--- backend/core/views/bookshelf.py
+++ backend/core/views/bookshelf.py
@@ -6,13 +6,15 @@ import json
 from django.http import JsonResponse
 from yaml import serialize
 from core import serializers
-from core.models.book import Book
+from core.models import Book, AnswerAssociativa
 from media.models.image import Image
 from core.serializers import BookshelfSerializer, BookSerializer
 from media.serializers import ImageSerializer
+import random
+# from core.views import AnswerAssociativaViewSet
 
 from core.models import User
-from core.models import Bookshelf, bookshelf
+from core.models import Bookshelf, Question, Genre
 from core.serializers import BookshelfSerializer
 
 class BookshelfViewSet(ModelViewSet):
@@ -20,7 +22,6 @@ class BookshelfViewSet(ModelViewSet):
     serializer_class = BookshelfSerializer
 
     def createBookshelf(request):
-
         if request.body:
             body = json.loads(request.body.decode('utf-8'))
 
@@ -45,8 +46,125 @@ class BookshelfViewSet(ModelViewSet):
 
             return HttpResponse("Estante cadastrada com sucesso!")
 
-        else:
-            return HttpResponse("Algo deu errado.")
+        # elif request:
+            
+        #     body = json.loads(request)
+
+        #     print(body)
+
+        #     bookshelf_name = body["bookshelf_name"]
+        #     bookshelf_desc = body["bookshelf_desc"]
+        #     user = body["user"]
+
+        #     user = User.objects.get(pk = user["id"])
+
+        #         # bookshelf = Bookshelf.objects.filter(bookshelf_name=bookshelf_name).first()
+            
+
+        #     bookshelf = Bookshelf.objects.create(
+        #         bookshelf_name=bookshelf_name,
+        #         booshelf_desc=bookshelf_desc,
+        #         user = user
+        #         )
+
+        #     bookshelf.save()
+
+        #     return HttpResponse("Estante cadastrada com sucesso!")
+
+    def createBookshelfQuiz(request):
+        if request:
+            body = json.loads(request)
+
+            # print(body)
+
+            bookshelf_name = body["bookshelf_name"]
+            bookshelf_desc = body["bookshelf_desc"]
+            user = body["user"]
+
+            user = User.objects.get(pk = user["id"])
+            bookshelf = Bookshelf.objects.filter(bookshelf_name=bookshelf_name).first() 
+            # bookshelf = Bookshelf.objects.filter(bookshelf_name=bookshelf_name).first()
+        
+
+            if bookshelf:
+                bookshelf_name=bookshelf_name,
+                bookshelf_desc=bookshelf_desc,
+                user = user
+                bookshelf.save()
+            else:
+                bookshelf = Bookshelf.objects.create(
+                bookshelf_name=bookshelf_name,
+                booshelf_desc=bookshelf_desc,
+                user = user
+                )
+                # id_user = (User.objects.get(username=username)).id
+                # perfil = User(username=username, user_biography=user_biography) #, cod_user=id_user)
+                #user.is_active = True
+                bookshelf.save()
+
+        
+        c = AnswerAssociativa.objects.filter(cod_resp_forms = user, cod_forms = 1 )
+        genre = Question.objects.filter(id = 1).prefetch_related('genres').all #sera que não estou usando na entidade errada? Sim eu estava, tenho que puxar esse atributo da entidade que eu o atribui
+        count_genre = Genre.objects.all()
+        count_question = Question.objects.all()
+        # print(genre)
+        # print(count_genre)
+        # print(genre(1).genres)
+        # a = Question.objects.all()
+        # print(c[0].cod_question)
+        # print(count_question[0].text)
+
+        # lista = []
+        # ordens = Question.objects.all()
+        # for ordem in ordens:
+        #     for o in ordem.genres.all():
+        #         lista.append(o.genres)
+        # print(lista[0])
+
+        # print(count_question[0].genres)
+
+
+        # for i in range(c.count()): #para cada elemento dos formulários associativos 
+        #     # 
+        #     # print(c[i].id) #mostrar o id elemento
+        #     for ii in range(count_genre.count()): #para cada item em questões
+        #         # print(count_genre[ii])
+        #         for iii in range(count_question.count()):
+        #             if str(c[i].cod_question) == str(count_question[iii].text):
+        #                 #tenho que descobrir aqui o gênero da questão
+
+
+        #                 print(count_question[iii].text )
+        #             else :
+        #                 print()
+                # if c[i].cod_question == a[ii]: #tenho que igualar o gênero da questão
+            #         print(genre[ii].genres) #ver se o código da questão bate com o código do item das questões
+            #         for iii in range(1,4):
+            #             if c[i].cod_answer == iii:
+            #                 continue
+
+
+
+
+
+        info = {'bookshelf_name' : bookshelf_name, 'bookshelf_desc' : bookshelf_desc, 'id' : 35 , 'user_id' : user.id }
+        # print(info)
+        BookshelfViewSet.addBookshelfQuiz(json.dumps(info))
+        return HttpResponse("Estante cadastrada com sucesso!")
+
+        # bookshelf_name
+        # : 
+        # "Estante com livros indicados"
+        # booshelf_desc
+        # : 
+        # "Estante com livros indicados"
+        # id
+        # : 
+        # 35
+        # user_id
+        # : 
+        # 1
+
 
     def getBookshelves(request, user_username):
         user_id = User.objects.get(username = user_username).id
@@ -86,13 +204,49 @@ class BookshelfViewSet(ModelViewSet):
 
     def addBookshelf(request):
         body = json.loads(request.body)
-
+        # print(body)
         book = Book.objects.get(pk = body["id_book"])
+        print(body["bookshelves"])
 
         for item in body["bookshelves"]:
+            # print(item)
             bookshelf = Bookshelf.objects.get(pk = item["id"])
             bookshelf.book.add(book)
 
+        return HttpResponse("Estantes atualizadas com sucesso!")
+        
+    def addBookshelfQuiz(request):
+        body = json.loads(request)
+        # print(body)
+        bookshelves = []
+
+        # 1. tenho que pegar informações das estantes do usuário
+        # for i in bookshelf
+        bookshelf = Bookshelf.objects.filter(user = 1)
+        for i in range(0,bookshelf.count()):
+            serializer = BookshelfSerializer(bookshelf[i]).data
+            bookshelves.append(serializer)
+         
+        # print(bookshelves)
+        #  
+        for i in range(1,4): #escolher a quantidade de livros que vai na bookshelf recém criada
+            # a = random.randrange(20) -> criar um contador aleatório para os livros para substituir id
+            
+
+            book = Book.objects.get(pk = i)
+            genre_books = Book.objects.prefetch_related('genre').filter(genre = 2) #Puxar o gênero escolhido no campo genre = x
+            # print(genre_books)
+            # print(book)
+            # print(genre_books.count())
+            for a in range(0,genre_books.count()):
+                if (book == genre_books[a]): 
+                    # print("oi")
+                    for item in bookshelves:
+                        bookshelf = Bookshelf.objects.get(pk = item["id"])
+                        bookshelf.book.add(book)
+                        # print(item)
+                else: 
+                    print("o que?")
         return HttpResponse("Estantes atualizadas com sucesso!")
         
 
